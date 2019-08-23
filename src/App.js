ce92b785fe9f4211f9fe90c3a0d4ef89c3baf428
Ming <lumingabby@outlook.com>
2019-08-22 23:30:02 -0400

    changed into component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {Component, setState, useEffect} from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Content from './Content';
@@ -13,113 +13,113 @@ import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 import Table from 'react-bootstrap/Table';
 
-function App() {
-  const [show, setShow] = useState(false);
-  const handleClick = () => setShow(true);
-  const handleClose = () => setShow(false);
+class App extends Component {
 
-  const [contents, setContents] = useState(
-    [
-        {
-          "id": 1,
-          "title": "Hello1",
-          "url":"World1"
-        }
-    ]
-  );
+  state={
+    show: false,
+    files: [
+      {
+        "id": 1,
+        "title": "Hello1",
+        "url":"World1"
+      }
+  ]
+  }
 
-  const addRow = () =>{
-    setContents(contents => {
-      contents = contents.concat(
+  handleClick = () => this.setState({show: true});
+  handleClose = () => this.setState({show: false});
+
+  addRow = () =>{
+    this.setState(prevState => ({
+      files: prevState.files.concat(
         {
           "id": 2,
           "title": "Hello2",
           "url":"World2"
         }
       )
-      return contents;
-    })
+    }))
   }
 
-  const deleteRow = (id) => {
-    setContents(contents => {
-      contents = contents.filter(content => {return content.id !== id});
-      return contents;
-    })
+  deleteRow = (id) => {
+    this.setState(prevState => ({
+      files: prevState.files.filter(files => {return files.id !== id})
+    }))
   }
 
-  useEffect(() => {
-    load();
-  },[contents]);
-  const load = () => {
-    // document.getElementById("content").appendChild(
-    //   "<h1> Hello World </h1>"
-    // );
+  // useEffect(() => {
+  //   load();
+  // },[contents]);
+  // const load = () => {
+  //   // document.getElementById("content").appendChild(
+  //   //   "<h1> Hello World </h1>"
+  //   // );
+  // }
+  render(){
+    return (
+      <Container>
+          <Jumbotron>
+            <h1 style={{"fontSize": 80}}>Design Manager</h1>
+            <p style={{"fontSize": 30}}> Manage your design here</p>
+          </Jumbotron>
+          <Row>
+            <Col xs={7}><h1>Designs</h1></Col>
+            <Col xs={2}></Col>
+            <Col><Button variant="primary" onClick={this.handleClick} block>Add a new design</Button></Col>
+          </Row>
+          
+          <Table>
+              <thead>
+                <tr>
+                <th width="10%">#</th>
+                <th width="25%">Title</th>
+                <th width="30%">URL</th>
+                <th width="35%">Action</th>
+                </tr>
+              </thead>
+                {
+                  this.state.files.map((content) =>{
+                    return (
+                      <Content id={content.id} title={content.title} url={content.url} deleteRow={this.deleteRow}/>
+                    );
+                  })
+                }
+          </Table>
+          
+             
+  
+        {/* Add Modal Area */}
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+          <Modal.Title>Upload Design</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group controlId="form.design">
+                <Form.Label>Design</Form.Label>
+                <Form.Control type="file"/>
+                <Form.Text className="text-muted">Please upload your design here</Form.Text>
+              </Form.Group>
+              <Form.Group controlId="form.description">
+                <Form.Label>Description</Form.Label>
+                <Form.Control type="text" placeholder="Please enter description here" />
+              </Form.Group>
+              
+            </Form>
+          </Modal.Body>
+  
+          <Modal.Footer>
+          <Button variant="secondary" onClick={this.handleClose}>
+            Cancel
+          </Button>
+          <Button variant="primary" onClick={() =>{this.addRow(); this.handleClose();}}>
+            Upload
+          </Button>
+          </Modal.Footer>
+        </Modal>
+      </Container>
+    );
   }
-
-  return (
-    <Container>
-        <Jumbotron>
-          <h1 style={{"fontSize": 80}}>Design Manager</h1>
-          <p style={{"fontSize": 30}}> Manage your design here</p>
-        </Jumbotron>
-        <Row>
-          <Col xs={7}><h1>Designs</h1></Col>
-          <Col xs={2}></Col>
-          <Col><Button variant="primary" onClick={handleClick} block>Add a new design</Button></Col>
-        </Row>
-        
-        <Table>
-            <thead>
-              <tr>
-              <th width="10%">#</th>
-              <th width="25%">Title</th>
-              <th width="30%">URL</th>
-              <th width="35%">Action</th>
-              </tr>
-            </thead>
-              {
-                contents.map((content) =>{
-                  return (
-                    <Content id={content.id} title={content.title} url={content.url} deleteRow={deleteRow}/>
-                  );
-                })
-              }
-        </Table>
-        
-           
-
-      {/* Add Modal Area */}
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-        <Modal.Title>Upload Design</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group controlId="form.design">
-              <Form.Label>Design</Form.Label>
-              <Form.Control type="file"/>
-              <Form.Text className="text-muted">Please upload your design here</Form.Text>
-            </Form.Group>
-            <Form.Group controlId="form.description">
-              <Form.Label>Description</Form.Label>
-              <Form.Control type="text" placeholder="Please enter description here" />
-            </Form.Group>
-            
-          </Form>
-        </Modal.Body>
-
-        <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
-          Cancel
-        </Button>
-        <Button variant="primary" onClick={() =>{addRow(); handleClose();}}>
-          Upload
-        </Button>
-        </Modal.Footer>
-      </Modal>
-    </Container>
-  );
 }
 
 export default App;
