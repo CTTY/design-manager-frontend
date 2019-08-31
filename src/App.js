759713165c41a94250189b3923f088ffef737834
Ming <lumingabby@outlook.com>
2019-08-30 23:24:09 -0400

    added carousel component into structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import MainPage from './DesignManagerFrontend/MainPage/MainPage';
+import Carousel from './ArtGallery/Carousel/Carousel';
 
 
 
 class App extends Component{
   render(){
     return (
-      <MainPage />
+      <React.Fragment>
+        <MainPage />
+        <Carousel />
+      </React.Fragment>
     );
   }  
 }
