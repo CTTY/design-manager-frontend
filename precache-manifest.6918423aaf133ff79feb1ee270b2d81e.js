4a9c5d81da20c35bb2cb5ae968339b7c4b10de5a
Ming <lumingabby@outlook.com>
2019-09-19 17:22:40 -0400

    Updates

--- precache-manifest.6918423aaf133ff79feb1ee270b2d81e.js
+++ precache-manifest.6918423aaf133ff79feb1ee270b2d81e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f4871e5a5593959ab838c257dba43991",
+    "revision": "e8e3441e98e746fcb86168b5cf857a20",
     "url": "/design-manager-frontend/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/design-manager-frontend/static/css/2.13645106.chunk.css"
   },
   {
-    "revision": "8bbc6541857bfe614a2e",
+    "revision": "80ad7b7471e0ee0b78a4",
     "url": "/design-manager-frontend/static/css/main.0111232e.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/design-manager-frontend/static/js/2.30eadc0f.chunk.js"
   },
   {
-    "revision": "8bbc6541857bfe614a2e",
-    "url": "/design-manager-frontend/static/js/main.c7b99016.chunk.js"
+    "revision": "80ad7b7471e0ee0b78a4",
+    "url": "/design-manager-frontend/static/js/main.dcef6238.chunk.js"
   },
   {
     "revision": "deaaedc084dafc5d8a3c",
