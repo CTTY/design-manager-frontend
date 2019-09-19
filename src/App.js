c908d4a764b353be250026ab7d4c7f226c09a6c2
Ming <lumingabby@outlook.com>
2019-09-19 14:32:39 -0400

    update demo designmngr

--- src/App.js
+++ src/App.js
@@ -95,6 +95,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={RBCarousel} />
           <Route exact path="/Email" component={Email} />
+          <Route exact path="/DesignManagerDemo" component={DesignMainPage} />
           <Route path="/login" 
                  render={(props) => <Login onLogin={this.handleLogin} {...props} />}></Route>
           <PrivateRoute authenticated={this.state.isAuthenticated} path="/DesignMainPage" component={DesignMainPage} handleLogout={this.handleLogout}></PrivateRoute>
